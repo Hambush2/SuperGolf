eed5230d5681f342a8c3ebd7182957da23c5ff40
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2021-11-09 17:21:36 +0000

    Added Ball events

--- SuperGolf/Assets/Scripts/Ball.cs
+++ SuperGolf/Assets/Scripts/Ball.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (TryGetValue(other, out Block() block))
+        //{
+
+        //}
+    }
 }
