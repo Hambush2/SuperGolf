f7faf7b29e4689a78bf7061bda0617538b949c3e
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2021-11-17 19:27:24 +0000

    fixed launching

---
+++ SuperGolf/Assets/Scripts/ZackScripts/Ball.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (TryGetValue(other, out Block() block))
+        //{
+
+        //}
+    }
+}
---
+++ SuperGolf/Assets/Scripts/ZackScripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private void Update()
+    {
+        //GameObject ball = (GameObject)FindObjectOfType(typeof(Ball));
+        //Vector3 position = ball.gameObject.transform.position;
+        //transform.position = position;
+    }
+}
