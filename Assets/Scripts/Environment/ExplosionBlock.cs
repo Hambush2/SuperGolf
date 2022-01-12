e2d67d64bbbb829f6fed6873bfc719fc6d3f93fc
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-01-12 23:16:39 +0000

    Added Explosion Audio

--- Assets/Scripts/Environment/ExplosionBlock.cs
+++ Assets/Scripts/Environment/ExplosionBlock.cs
@@ -7,7 +7,14 @@ public class ExplosionBlock : MonoBehaviour
 {
     private Explosion _explosion;
     private bool _isActivated = false;
+
+    [Header("Particles")]
     [SerializeField] private GameObject _explosionParticle;
+
+    [Header("Audio Effect")]
+    [SerializeField] private AudioSource _explosionAudio = null;
+
+    private bool _audioPlayed = false;
 
     private void Awake()
     {
@@ -20,6 +27,21 @@ public class ExplosionBlock : MonoBehaviour
         {
             _explosion.Explode();
             Instantiate(_explosionParticle, transform.position, Quaternion.identity);
+            
+            if(!_audioPlayed)
+            {
+                _explosionAudio.Play();
+                _audioPlayed = true;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.TryGetComponent(out Ball ball))
+        {
+            _explosionAudio.Play();
         }
     }
 
