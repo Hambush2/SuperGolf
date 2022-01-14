cd26d2fe373e74b74fc5af497ab15a87f48b930c
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2022-01-14 14:56:56 +0000

    Added Slingshot controller and animation for PC

---
+++ Assets/Scripts/ControllerScripts/FinalControllerPC.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalControllerPC : MonoBehaviour
+{
+    //Establish Variables
+
+    //position vector
+    public Vector2 whilePos;
+
+    //distance pulled back
+    public float pullDis;
+
+    //distance and angle pulled left or right/up or down
+    public float startPos;
+    public float lrPullDis;
+
+    //ball object to launch
+    public GameObject ball;
+    public GameObject iball;
+
+    //canon offset
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            startPos = Screen.height / 2;
+            whilePos = Input.mousePosition;
+            pullDis = whilePos.y - startPos;
+            lrPullDis = whilePos.x - Screen.width / 2;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (pullDis < 0)
+            {
+                //launch the ball
+                Launch();
+            }
+        }
+    }
+
+    void Launch()
+    {
+        //instantiate a copy of the ball object
+        iball = Instantiate(ball, this.transform.position + offset, Quaternion.identity);
+        //send it with force from input
+        iball.GetComponent<Rigidbody>().AddForce(new Vector3(pullDis, -pullDis, -lrPullDis), ForceMode.Impulse);
+        pullDis = 0;
+    }
+
+}
