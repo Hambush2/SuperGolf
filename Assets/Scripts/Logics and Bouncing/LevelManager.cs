3ec7136d390b19822292deebed97fe6553f19917
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2022-01-15 22:03:25 +0000

    Made Game Pause When Game Complete

--- Assets/Scripts/Logics and Bouncing/LevelManager.cs
+++ Assets/Scripts/Logics and Bouncing/LevelManager.cs
@@ -7,11 +7,15 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private float _winningCondition;
     [SerializeField] private GameOverPopup _GameOverPopup;
 
+    public GameObject cannon;
+
     private void Update()
     {
         if (Coin.Count > _winningCondition)
         {
+            Time.timeScale = 0;
             GameObject.FindObjectOfType<Timer>().enabled = false;
+            cannon.GetComponent<FinalControllerPC>().enabled = false;
             _GameOverPopup.setCondition(true);
             _GameOverPopup.Open();
         }
