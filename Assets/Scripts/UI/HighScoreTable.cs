e5c9317e0a9615e98c951eec1a84ba30521ef7e2
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-04-07 20:28:20 +0100

    Updated High Score model

--- Assets/Scripts/UI/HighScoreTable.cs
+++ Assets/Scripts/UI/HighScoreTable.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class HighScoreTable : MonoBehaviour
 {
-    private Transform _entryContainer;
-    private Transform _entryTemplate;
+    [SerializeField] private Transform _entryContainer;
+    [SerializeField] private Transform _entryTemplate;
 
     private void Awake()
     {
-        _entryContainer = transform.Find("");
-        _entryTemplate = transform.Find("HighScoreTemplate");
 
         _entryTemplate.gameObject.SetActive(false);
 
+        float templateHeight = 10f;
+        for(int i = 0; i < 5; i++)
+        {
+            Transform entryTransform = Instantiate(_entryTemplate, _entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
     }
 }
